ade393c17f6e27df2788ae7b9a0bca805480ce7b
majo06005 <majo06005@E-PW0234K2>
2023-09-06 10:17:43 +0200

    Add project files.

---
+++ Inlämningsupgift1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1");//Skriver att jag nästan är klar med kapitel 1.
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \\");//Skriver om nya tecken.
+        }
+    }
+}
+
